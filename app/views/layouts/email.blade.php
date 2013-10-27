bfcce85221111fb08261f35c1f6420689ed4a4dd
unisys12 <unisys12@gmail.com>
2013-10-27 17:05:31 -0500

    Layout Files - Initial Commit
    Bring in pre-made layout files

---
+++ app/views/layouts/email.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<!--[if IE 8]> 				 <html class="no-js lt-ie9" lang="en" > <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en" > <!--<![endif]-->
+	<head>
+		<meta charset="utf-8" />
+		<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+		<title>Rayco Inc; Authorized Savin Dealer</title>
+		<meta name="description" content="Rayco Inc; Independent Savin Dealer; Corinth, MS" />
+		<meta name="author" content="Phillip Jackson" />
+
+		<!-- Set view port for responsiveness of the site -->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+		<!-- Load Rayco Favicon -->
+		<link rel="shortcut icon" href="favicon.ico" />
+
+		<!-- Calls to the single compiled and compressed CSS file -->
+		<link rel="stylesheet" href="{{ asset('css/app.css') }}" />
+	</head>
+<body>
+	<header class="row">
+		<section class="large-6 columns" >
+			<figure>
+				<img src="{{ asset('images/logos/Rayco_logo_bw.png') }}" class="rayco" alt="Rayco Logo"/>
+			</figure>
+			<figure>
+				<img src="{{ asset('images/logos/savin_logo.png') }}" class="savin" alt="Savin Logo" />
+			</figure>
+		</section>
+		<nav class="large-6 columns">
+			<ul class="inline-list">
+				<li><a href="{{ route('home') }}">Home</a></li>
+				<li><a href="{{ route('products') }}">Products</a>
+					<!-- <ul class="subMenu">
+						<li><a href="http://www.raycocopiers.com/products/bwmfp.php">B\W MFPs</a></li>
+						<li><a href="http://www.raycocopiers.com/products/bwprinters.php">B\W Printers</a></li>
+						<li><a href="http://www.raycocopiers.com/products/colormfp.php">Color MFPs</a></li>
+						<li><a href="http://www.raycocopiers.com/products/colorprinters.php">Color Printers</a></li>
+						<li><a href="http://www.raycocopiers.com/products/wideformat.php">Wide Format</a></li>
+						<li><a href="http://www.raycocopiers.com/products/scanner.php">Scanners</a></li>
+					</ul> --></li>
+				<li><a href="http://www.raycocopiers.net">Services</a></li>
+				<li><a href="{{ route('customers') }}">Customers</a></li>
+				<li><a href="{{ route('about') }}">About Us</a></li>
+			</ul>
+		</nav>
+		<hr />
+	</header>
+	<main class="row">
+
+		@yield('content')
+
+	</main>
+	<footer class="row">
+		<section class="large-12 columns">
+			<h3>Contact Us</h3>
+			<ul>
+				<li>Phone: (662)287-8829</li>
+				<li>Fax: (662)286-3354</li>
+				<li>Email: <a href = "mailto:jcooper@raycocopiers.com">info@raycocopiers.com</a></li>
+				<li>2199 Hwy 72 East; Corinth, MS 38834</li>
+				<li><small>&copy; Copyright  by Phillip Jackson</small></li>
+			</ul>
+
+			<a href="{{ route('home') }}">Home</a> - <a href="{{ route('products') }}">Products</a> - <a href="http://www.raycocopiers.com/comingsoon.html">Services</a> - <a href="{{ route('customers') }}">Customers</a> - <a href="{{ route('about') }}">About Us</a>
+		</section>
+	</footer>
+</body>
+</html>
