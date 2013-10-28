e8a35209a1fe47d08c8e09f2eb574b7fcfa3aa43
unisys12 <unisys12@gmail.com>
2013-10-27 21:39:26 -0500

    Create Customer Index Page
    Dummy text for now

---
+++ app/views/customers/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.master')
+
+@section('content')
+
+<section class="large-8 columns">
+	<header>
+		<h3>Customers and Stuff</h3>
+		<br>
+		<br>
+		<article class="content">
+			<p>Even more useless info!</p>
+		</article>
+	</header>
+</section>
+
+@stop
