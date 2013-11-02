82363a26292f292d27d1cc8696853a51804aa60c
unisys12 <unisys12@gmail.com>
2013-11-02 16:45:47 -0500

    updated to reflect new classifications of products

--- app/views/products/index.blade.php
+++ app/views/products/index.blade.php
@@ -7,7 +7,7 @@
 		<h3></h3>
 	</header>
 	<nav class="ul">
-		<li><a href="#">Black and White MPFs</a></li>
+		<li><a href="/products/bw/mfp">Black and White MPFs</a></li>
 		<li><a href="#">Black and White Printers</a></li>
 		<li><a href="#">Color MFPs</a></li>
 		<li><a href="#">Color Printers</a></li>
