3a5dbe8e17284564af06a012b2dd31a2a8134891
unisys12 <unisys12@gmail.com>
2013-11-03 08:31:07 -0600

    update product family template

--- app/views/products/family.blade.php
+++ app/views/products/family.blade.php
@@ -2,14 +2,16 @@
 
 @section('content')
 
-<section class="large-8 columns family">
-	<header class="family_header">
+<section class="large-8 columns">
+	<header>
 		<h3> {{ strtoupper($family) }} </h3>
 	</header>
-	<article class="family_content">
+	<article class="family">
 		@foreach($product as $item)
 		<img src= "/raycocopiers/public/images/products/{{ $item->family }}/{{ $item->color_class }}/{{ $item->name }}.jpg" alt="{{ $item->name }}">
 		<h6>{{ $item->name }}</h6>
+		<a href="{{ $item->family }}/{{ $item->color_class }}/{{ $item->name }}">Want more info?</a>
+		<hr>
 		@endforeach
 	</article>
 </section>
