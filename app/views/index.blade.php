fd4e48b8d4caf7676fb97facb8953ed9bd83d7f5
unisys12 <unisys12@gmail.com>
2013-11-09 20:50:05 -0600

    update index view with carousel images

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -2,44 +2,19 @@
 
 @section('content')
 	<section class="large-8 columns">
-		<header class="content_header">
-			<h3>Rayco Inc</h3>
-		</header>
 
-		<article class="content">
-			<p>
-				<em>Rayco Inc</em> has offices in Corinth, Oxford and Tupelo Mississippi. We are
-				Mississippi owned company that started in 1994. There are a lot of changes coming from
-				Ikon going out of business and a Nashville company buying the contacts from Ricoh Direct.
-				This will effect anyone who has any Ikon equipment (Ricoh / Cannon). We want to let you
-				know we are ready to assist you in any way to help provide service and supplies to you.
-				We can provide service contracts or repair by the call. Also we have network people if
-				you need any I.T. assistance. We hope we can work together to keep your to keep your money
-				in Mississippi.
-			</p>
-			<p>
-				Please read all documents you receive very carefully. If we can be of any assistance, please
-				call us at 1-888-873-5789.
-			</p>
+		<article class="carousel">
+			<figure class="image-one">
+				<img src=" {{ asset('images/banners/multifunction.jpg') }} " alt="Multifunction Banner">
+			</figure>
 
-			<p>
-				Having choices on who to call is the only way to get the best service and lowest price on
-				your office equipment. If you have service included in your Ikon lease, you will not have
-				a choice on your service provider. They will contact you very soon. Rayco can replace
-				your leased equipment before the end of your lease, so please give us a call to find out
-				the details.
-			</p>
-			<p>
-				We are Rayco want you to know you have a choice in North Mississippi on your office
-				equipment sales and service.
-			</p>
-			<p>
-				Ikon was a very good member of the community and did a very good job of providing quality
-				service to many businesses in this area. They were also a very good competitor for us.
-				Ikon and its employees will be missed in Mississippi.
-			</p>
-			<p>Sincerely,<br>
-			<p>Rayco Inc Ownership</p>
+			<figure class="image-two">
+				<img src=" {{ asset('images/banners/prod-pg-banner-sample2.jpg') }} " alt="TimeSaving Banner">
+			</figure>
+
+			<figure class="image-three">
+				<img src=" {{ asset('images/banners/solutions-banner-2.jpg') }} " alt="Solutions Banner">
+			</figure>
 		</article>
 	</section>
 @stop
