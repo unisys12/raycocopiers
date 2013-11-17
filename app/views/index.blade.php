4bc285c1a9f18373822f10f5c9083182280abc2a
unisys12 <unisys12@gmail.com>
2013-11-16 18:38:57 -0600

    uppercase image names
    Required by host... BOO!

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -5,17 +5,17 @@
 
 		<article class="carousel">
 			<figure class="image img-one">
-				<img src=" {{ asset('images/banners/multifunction.jpg') }} " alt="Multifunction Banner">
+				<img src=" {{ asset('images/banners/Multifunction.jpg') }} " alt="Multifunction Banner">
 				<figcaption>Rayco offers a full line of Savin Black&White and Color MPFs. Please, take a look around and let's us know any questions you might have.</figcaption>
 			</figure>
 
 			<figure class="image img-two">
-				<img src=" {{ asset('images/banners/prod-pg-banner-sample2.jpg') }} " alt="TimeSaving Banner">
+				<img src=" {{ asset('images/banners/Prod-Pg-Banner-Sample2.jpg') }} " alt="TimeSaving Banner">
 				<figcaption>Looking to save time and money? Who isn't? Well, take a look at our products and see what you can find</figcaption>
 			</figure>
 
 			<figure class="image img-three">
-				<img src=" {{ asset('images/banners/solutions-banner-2.jpg') }} " alt="Solutions Banner">
+				<img src=" {{ asset('images/banners/Solutions-Banner-2.jpg') }} " alt="Solutions Banner">
 				<figcaption>What your office to go paperless? Don't know what paperless is or have questions and concerns? Feel free to contact us and we will be happy to help any way we can.</figcaption>
 			</figure>
 		</article>
