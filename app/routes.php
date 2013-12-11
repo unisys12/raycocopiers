eefe81a73176a13df8b738b9c0dd1c88efc058b5
unisys12 <unisys12@gmail.com>
2013-12-10 18:27:20 -0600

    remove useless route

--- app/routes.php
+++ app/routes.php
@@ -25,9 +25,7 @@ Route::get('/about', array('as' => 'about', function()
 /* Customer Routes
  * (http://www.raycocopiers.com/customers)
  * ****************/
-Route::post('/customers/metersubmit', array('as' => 'metersubmited', 'uses' => 'CustomerController@meterSubmit'));
-
-Route::get('/customers/metersubmit', array('as' => 'metersubmit', 'uses' => 'CustomerController@meterSubmit'));
+Route::post('/customers/metersubmit', array('as' => 'metersubmit', 'uses' => 'CustomerController@meterSubmit'));
 
 Route::get('/customers/meter', array('as' => 'meter', 'uses' => 'CustomerController@showMeterForm' ));
 
