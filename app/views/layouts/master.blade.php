7d1f220e825794f26c960c41dddef31286f6e1f1
unisys12 <unisys12@gmail.com>
2014-01-12 22:10:11 -0600

    New Universal Analytics Script

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -87,20 +87,16 @@
 	</footer>
 <!-- End Main Content/Container -->
 
-	<script type="text/javascript">
-	  var _gaq = _gaq || [];
-	  var pluginUrl = '//www.google-analytics.com/plugins/ga/inpage_linkid.js';
-	  _gaq.push(['_require', 'inpage_linkid', pluginUrl]);
-	  _gaq.push(['_setAccount', 'UA-36848345-1']);
-	  _gaq.push(['_setDomainName', 'raycocopiers.com']);
-	  _gaq.push(['_trackPageview']);
+	<!-- New Universal Analytics Script -->
+		<script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-	  (function() {
-	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	  })();
-	</script>
+         ga('create', 'UA-36848345-1', 'raycocopiers.com');
+         ga('send', 'pageview');
+        </script>
 
 	</body>
 </html>
